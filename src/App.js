d419c9ca4f172bd720bf3738908fff5eee64541c
gopalakrishnan <kingkrishna809@gmail.com>
2026-02-21 23:41:57 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/home/home";
-import Gokul from "./components/pages/gokul";
-import Vinoth from "./components/pages/vinoth";
 import AppNavbar from "./components/navbar/navbar";
 
 function App() {
@@ -10,9 +8,7 @@ function App() {
     <Router>
       <AppNavbar />
       <Routes>
-         <Route path="/memories/home" element={<Home />} />
-        <Route path="/memories/gokul" element={<Gokul />} />
-        <Route path="/memories/vinoth" element={<Vinoth />} />
+         <Route path="/memories" element={<Home />} />
       </Routes>
     </Router>
   );
