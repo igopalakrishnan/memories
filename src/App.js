d34bd467df27026a223ed15f2d7e87b6dd432f70
gopalakrishnan <kingkrishna809@gmail.com>
2026-02-21 23:07:02 +0530

    update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <AppNavbar />
       <Routes>
-         <Route path="/" element={<Home />} />
+         <Route path="/home" element={<Home />} />
         <Route path="/gokul" element={<Gokul />} />
         <Route path="/vinoth" element={<Vinoth />} />
       </Routes>
