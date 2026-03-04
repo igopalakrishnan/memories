c6e9a23aa42b72eeb6677912c9e897e432794b66
gopalakrishnan <kingkrishna809@gmail.com>
2026-03-04 11:22:19 +0530

    update

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,13 +5,8 @@ import "./navbar.css";
 
 const AppNavbar = () => {
   return (
-    <Navbar className="navbar"  variant="dark" expand={false}>
+    <Navbar className="navbar" variant="dark" expand={false}>
       <Container fluid className="d-flex align-items-center">
-        {/* Left-aligned clickable icon */}
-        <Link to="/" className="navbar-icon">
-          <i className="bi bi-house-door-fill"></i>
-        </Link>
-
         {/* Centered HOME text */}
         <Navbar.Brand href="/memories" className="mx-auto text-center">
           HOME
